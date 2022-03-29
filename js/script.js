935be77c89554ef06c3799688ec3138d1925d5af
paige1996 <guni33@naver.com>
2022-03-29 13:17:36 +0800

    Added list

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@
 
 //1. find element
 let addTaskButton = document.getElementById("add-task")
-let newTaskInput = document.getElementById("task")
+let newTaskInput = document.getElementById("task-input")
+let todoListContainer = document.getElementById("todo-list")
 //2. Write a function to implement the behavior
-function onAddTaksClicked(event) {
+function onAddTaskClicked(event) {
     let taskName = newTaskInput.value;
     newTaskInput.value = "";
     console.log(taskName)
+    todoListContainer.insertAdjacentHTML("afterbegin", taskName);
 }
 
 //3. add event listener. link element, function and name
-addTaskButton.addEventListener('click', onAddTaksClicked)
+addTaskButton.addEventListener('click', onAddTaskClicked)
