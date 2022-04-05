aeaf3e49c65eb13ddfb51fad2febb23efec36cdb
paige1996 <guni33@naver.com>
2022-04-05 12:14:09 +0800

    Added show all tasks

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 // Empty script file to start with
 
-//1. find element
+//1. find element 1. find the button! find the class/id/etc
 let addTaskButton = document.getElementById("add-task")
 let newTaskInput = document.getElementById("task-input")
 let todoListContainer = document.getElementById("todo-list")
+let showActiveButton = document.getElementById("show-active")
+let showAllButton = document.getElementById("show-all")
 
 let templateElement = document.getElementById("list-item-template")
 let template = templateElement.innerHTML
 
 
-//2. Write a function to implement the behavior
+//2. Write a function to implement the behavior 2. write function
 function onAddTaskClicked(event) {
     let taskName = newTaskInput.value;
     newTaskInput.value = "";
@@ -19,7 +21,7 @@ function onAddTaskClicked(event) {
 }
 
 function onTodoListClicked(event) {
-    let targetElement = event.toElement;
+    let targetElement = event.target;
 
     while (!targetElement.classList.contains("task")) {
         targetElement = targetElement.parentElement
@@ -33,6 +35,31 @@ function onTodoListClicked(event) {
     }
 }
 
+function showActiveTasks() {
+    let tasks = document.getElementsByClassName("task")
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].classList.contains("completed")) {
+            tasks[i].style.display = "none"
+        }
+        else {
+            tasks[i].style.display = "block" //to show an element, set show all. block을 모두 보여줌
+        }
+
+    }
+}
+
+function showAllTasks() {
+    let tasks = document.getElementsByClassName("task")
+    for (let i = 0; i < tasks.length; i++) {
+        tasks[i].style.display = "block"
+    }
+}
+
 //3. add event listener. link element, function and name
-addTaskButton.addEventListener('click', onAddTaskClicked)
-todoListContainer.addEventListener('click', onTodoListClicked)
+addTaskButton.addEventListener('click', onAddTaskClicked);
+todoListContainer.addEventListener('click', onTodoListClicked);
+
+showActiveButton.addEventListener('click', showActiveTasks)
+
+
+showAllButton.addEventListener('click', showAllTasks)
