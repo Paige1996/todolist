c8518093a4ecfd83ccf0c3081a9f808996df9e6a
paige1996 <guni33@naver.com>
2022-04-05 11:27:05 +0800

    Added more js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 let addTaskButton = document.getElementById("add-task")
 let newTaskInput = document.getElementById("task-input")
 let todoListContainer = document.getElementById("todo-list")
+
 let templateElement = document.getElementById("list-item-template")
 let template = templateElement.innerHTML
 
@@ -17,5 +18,21 @@ function onAddTaskClicked(event) {
     todoListContainer.insertAdjacentHTML("afterbegin", taskHTML);
 }
 
+function onTodoListClicked(event) {
+    let targetElement = event.toElement;
+
+    while (!targetElement.classList.contains("task")) {
+        targetElement = targetElement.parentElement
+    }
+    let checkbox = targetElement.querySelector(".checkbox");
+    if (checkbox.checked) {
+        targetElement.classList.add("completed");
+    }
+    else {
+        targetElement.classList.remove("completed");
+    }
+}
+
 //3. add event listener. link element, function and name
 addTaskButton.addEventListener('click', onAddTaskClicked)
+todoListContainer.addEventListener('click', onTodoListClicked)
